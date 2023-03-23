a72904a452d1694bdf972da0c0d864ace9c459f6
Arpit Gulati <gulati.arpit0055@gmail.com>
2023-03-23 10:38:37 +0530

    for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const cors = require('cors')
 const bodyParser = require('body-parser');
 
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["http://localhost:3000" , "https://pricewarrior.netlify.app"]
+  }
+));
 app.use(express.json());
 // app.use(bodyParser.urlencoded({
 //   extended: true
