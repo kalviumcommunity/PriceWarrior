5b0cedeb359001828331769208a06c567ba0fcc4
Arpit Gulati <gulati.arpit0055@gmail.com>
2023-03-23 14:15:55 +0530

    added loading

--- frontend/src/components/GameComponent/gameFinal.js
+++ frontend/src/components/GameComponent/gameFinal.js
@@ -1,77 +1,98 @@
 import Game from "./gameTop/Game";
 import Nav from "./gameSectionNav/Nav";
 import Prices from "./gameparts/Prices";
-
+import axios from 'axios'
 import About from "./gameparts/About";
 import { useParams } from "react-router-dom";
-// import particularData from "../HomeComponents/UpcomingGames/gameData";
 import { useState, useEffect } from "react";
+import loadin from '../asseets/loadinghd.gif'
 
 function GameFinal() {
   const { id } = useParams();
   console.log(id);
   const [gameData, setGameData] = useState();
+  const [loading, setLoading] = useState(true)
   const [selectedButton, setSelectedButton] = useState("Prices");
   // useEffect(() => console.log(gameData), [gameData])
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_DataBase_link_to_Access_data}/gameGet/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setGameData(data);
-      });
+    const apiCall = async ()=>{
+      const res=  await axios.get(`${process.env.REACT_APP_DataBase_link_to_Access_data}/gameGet/${id}`)
+        
+      setGameData(res.data);
+      setTimeout(()=>{
+        setLoading(false)
+      },3000)
+      
+        
+
+    }
+    apiCall();
   }, [id]);
+
+  
 
   // let detail = particularData[Number(id) - 1];
   return (
-    gameData && (
-      <div className="gamefinal">
-        <Game
-          name={gameData.name}
-          lastName={gameData.lastName}
-          img={gameData.detailImage}
-          edition={gameData.editions}
-          rating={gameData.rating}
-          currentMin={gameData.currentMin}
-          currentAvg={gameData.currentAvg}
-          currentMax={gameData.currentMax}
-          historyMin={gameData.historyMin}
-          historyAvg={gameData.historyAvg}
-          historyMax={gameData.historyMax}
-          carouselImage={gameData.crouselImage}
-        />
+    <div className="gamefinal">
+      
+    {loading ? (
+      <div className="loading-ani">
+     <img width='250' src={loadin}/>
+     <h2>Loading...</h2>
+     </div>
+    ):(
+ <div className="gamefinal">
+ <Game
+   name={gameData.name}
+   lastName={gameData.lastName}
+   img={gameData.detailImage}
+   edition={gameData.editions}
+   rating={gameData.rating}
+   currentMin={gameData.currentMin}
+   currentAvg={gameData.currentAvg}
+   currentMax={gameData.currentMax}
+   historyMin={gameData.historyMin}
+   historyAvg={gameData.historyAvg}
+   historyMax={gameData.historyMax}
+   carouselImage={gameData.crouselImage}
+ />
 
-        <Nav setSelectedButton={setSelectedButton} />
+ <Nav setSelectedButton={setSelectedButton} />
 
-        {selectedButton === "Prices" && (
-          <Prices
-            img={gameData.detailImage}
-            releaseDate={gameData.releaseDate}
-            developer={gameData.developer}
-            publisher={gameData.publisher}
-            genres={gameData.genres}
-            tags={gameData.tags}
-            prices={gameData.prices}
-            carouselImage={gameData.crouselImage}
-          />
-        )}
+ {selectedButton === "Prices" && (
+   <Prices
+     img={gameData.detailImage}
+     releaseDate={gameData.releaseDate}
+     developer={gameData.developer}
+     publisher={gameData.publisher}
+     genres={gameData.genres}
+     tags={gameData.tags}
+     prices={gameData.prices}
+     carouselImage={gameData.crouselImage}
+   />
+ )}
 
-        {selectedButton === "About" && (
-          <About
-            description={gameData.description}
-            relatedLinks={gameData.relatedLinks}
-            minimumRequirements={gameData.minimumRequirements}
-            recommendedRequirements={gameData.recommendedRequirements}
-            trailer={gameData.trailer}
-            releaseDate={gameData.releaseDate}
-            developer={gameData.developer}
-            publisher={gameData.publisher}
-            genres={gameData.genres}
-            tags={gameData.tags}
-          />
-        )}
-      </div>
-    )
+ {selectedButton === "About" && (
+   <About
+     description={gameData.description}
+     relatedLinks={gameData.relatedLinks}
+     minimumRequirements={gameData.minimumRequirements}
+     recommendedRequirements={gameData.recommendedRequirements}
+     trailer={gameData.trailer}
+     releaseDate={gameData.releaseDate}
+     developer={gameData.developer}
+     publisher={gameData.publisher}
+     genres={gameData.genres}
+     tags={gameData.tags}
+   />
+ )}
+</div>
+    )}
+    
+     
+    
+  </div>
   );
 }
 
--- frontend/src/components/GameComponent/gameMainPage/mainGameCarousel.js
+++ frontend/src/components/GameComponent/gameMainPage/mainGameCarousel.js
@@ -4,19 +4,47 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { Link } from "react-router-dom";
 import GameList from "./GameMain";
+import axios from "axios";
+import loadin from '../../asseets/loadinghd.gif'
 
 function MainGameHome() {
+  const [List, setList] = useState([]);
   const [popular, setPopular] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const apiGameCall = async ()=>{
+      const res = await axios.get(`${process.env.REACT_APP_DataBase_link_to_Access_data}/gameMainGet`)
+       setList(res.data);
+       setTimeout(()=>{
+         setLoading(false)
+
+       },3000)
+    }
+    apiGameCall()
+  }, []);
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_DataBase_link_to_Access_data}/gameMainGet`)
       .then((res) => res.json())
+
       .then((data) => setPopular(data));
   }, []);
 
   return (
-    <div className="parts">
-      <GameList />
+    <div className="gamefinal">
+      {loading ?(
+         <div className="loading-ani">
+         <img width='250' src={loadin}/>
+         <h2>Loading...</h2>
+         </div>
+      ):(
+        <div className="parts">
+        <GameList List = {List} setList = {setList} />
+      </div>
+      )}
+
+   
     </div>
   );
 }
