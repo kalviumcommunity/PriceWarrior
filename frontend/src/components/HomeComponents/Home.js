127172b386025fbe705051ef55f673f9d5efa5d2
Arpit Gulati <gulati.arpit0055@gmail.com>
2023-04-10 19:19:22 +0530

    Some Changes

--- frontend/src/components/HomeComponents/Home.js
+++ frontend/src/components/HomeComponents/Home.js
@@ -3,9 +3,7 @@ import HomeTop from "./HomeTopComponent/HomeTop";
 import LatestGame from "./LatestGames/LatestGame";
 import UpcomingGames from "./UpcomingGames/UpcomingGames";
 import Something from "./Something/Something";
-import { useState, useEffect } from "react";
-import axios from 'axios'
-import loadin from '../asseets/loadinghd.gif'
+
 
 
 function Home({mainRef, data}) {
