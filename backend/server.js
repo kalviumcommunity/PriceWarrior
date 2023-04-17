3289a1755efb4938633d3d5033880cdb5de76450
Arpit Gulati <gulati.arpit0055@gmail.com>
2023-04-17 13:27:13 +0530

    added Spline

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors')
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 
 app.use(cors(
---
+++ frontend/src/components/errorPage/ErrorPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline'
+
+function ErrorPage() {
+  return (
+    <div style={{height:'60vh'}}>
+        <Spline 
+          className='Spline'
+          scene='https://prod.spline.design/s-Ju3hpzU123GPax/scene.splinecode'
+        />
+    </div>
+  )
+}
+
+export default ErrorPage
